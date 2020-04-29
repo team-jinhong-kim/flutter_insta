decaa7279736ca1686a6e67204b514e510c0348d
team-jinnhong-kim <team.jinhong.kim@gmail.com>
2020-04-30 02:31:47 +0900

    adding gridview and nopost image on post tab

--- lib/widgets/post.dart
+++ lib/widgets/post.dart
@@ -3,6 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttershare/models/user.dart';
 import 'package:fluttershare/pages/home.dart';
+import 'package:fluttershare/widgets/custom_image.dart';
 import 'package:fluttershare/widgets/progress.dart';
 
 class Post extends StatefulWidget {
@@ -122,8 +123,11 @@ class _PostState extends State<Post> {
     return GestureDetector(
       onDoubleTap: () => print('liking post'),
       child: Stack(
-          alignment: Alignment.center,
-          children: <Widget>[Image.network(mediaUrl)]),
+        alignment: Alignment.center,
+        children: <Widget>[
+          cachedNetworkImage(mediaUrl),
+        ],
+      ),
     );
   }
 
